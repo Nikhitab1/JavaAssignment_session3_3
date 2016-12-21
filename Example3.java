7cb7b652489fc7fd76c42ea8dabe7e123da1dd17
Nikhitab1 <b.nikhitha.sai@gmail.com>
2016-12-21 23:03:26 +0530

    Add files via upload

---
+++ Example3.java
@@ -0,0 +1,17 @@
+
+ class Example3 {
+	 
+	 int x;
+	 int y;
+	 String name;
+	 
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Example3 pnt = new Example3();
+       System.out.println("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y); //Since the values are not initiilised , it prints default values as null for string 
+                                        // and 0 for integer values
+	}
+
+}
